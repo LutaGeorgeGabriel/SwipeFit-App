59fe3b72474a5a6776d977f18b096df9d1992a17
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-22 20:29:44 +0300

    added the possibility to retrieve data from an API rather than the local data.json

--- app/src/main/java/eu/swipefit/app/app/swiping/SwipingActivity.java
+++ app/src/main/java/eu/swipefit/app/app/swiping/SwipingActivity.java
@@ -4,16 +4,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.daprlabs.aaron.swipedeck.SwipeDeck;
 import com.gjiazhe.multichoicescirclebutton.MultiChoicesCircleButton;
 
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import eu.swipefit.app.Product;
 import eu.swipefit.app.R;
-import eu.swipefit.app.Utils;
+import eu.swipefit.app.networking.Networking;
 import eu.swipefit.app.app.favorites.FavoritesContainer;
 import me.imid.swipebacklayout.lib.SwipeBackLayout;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
@@ -29,30 +36,48 @@ public class SwipingActivity extends Activity implements SwipeBackActivityBase{
     private SwipeBackActivityHelper swipeBackActivityHelper;
     private SwipeDeck swipeDeck;
     private ProductCard card;
-    private Context context;
-    private ArrayList<ProductCard> cards = new ArrayList<>();
+    private List<ProductCard> cards = new ArrayList<>();
+    public static Context context = null;
+    private static List<Product> products = new ArrayList<>();
+
     private static int cardIndex = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.swiping_activity);
+        context = getApplicationContext();
+        Properties properties = new Properties();
+        try {
+            properties.load(this.getApplication().getAssets().open("app.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String URL = properties.getProperty("URL");
+
+        ProductsAsyncTask productsAsyncTask = (ProductsAsyncTask) new ProductsAsyncTask(new ProductsAsyncTask.AsyncResponse() {
+            @Override
+            public void processFinish(List<Product> list) {
+                products = list;
+            }
+        }).execute(URL);
+
         swipeBackActivityHelper = new SwipeBackActivityHelper(this);
         swipeBackActivityHelper.onActivityCreate();
-        setContentView(R.layout.swiping_activity);
         final SwipeDeck cardStack = findViewById(R.id.swipe_deck);
 
-        for(int i = 0; i < Utils.loadProfiles(this.getApplicationContext()).size();i++) {
-            card = new ProductCard(swipeDeck, Utils.loadProfiles(this.getApplicationContext()).get(i),context);
+        for(int i = 0; i < products.size(); i++) {
+            card = new ProductCard(swipeDeck, products.get(i),context);
             card.setIndex(i);
             cards.add(card);
         }
 
-        final List testData = new ArrayList<>();
+      /*  final List testData = new ArrayList<>();
         testData.add("0");
         testData.add("1");
         testData.add("2");
         testData.add("3");
         testData.add("4");
-
+*/
         final SwipeDeckAdapter adapter = new SwipeDeckAdapter(cards,this);
         if(cardStack != null){
             cardStack.setAdapter(adapter);
@@ -149,6 +174,30 @@ public class SwipingActivity extends Activity implements SwipeBackActivityBase{
     @Override
     public void scrollToFinishActivity() {
 
+    }
+
+    private static class ProductsAsyncTask extends AsyncTask<String,Void,List<Product>> {
+
+        public interface AsyncResponse {
+            void processFinish(List<Product> list);
+        }
+
+        public AsyncResponse delegate = null;
+
+        public ProductsAsyncTask(AsyncResponse delegate){
+            this.delegate = delegate;
+        }
+
+        @Override
+        protected List<Product> doInBackground(String... strings) {
+            List<Product> listOfProducts = Networking.fetchEarthquakeData(context ,strings[0]);
+            return listOfProducts;
+        }
+
+        @Override
+        protected void onPostExecute(List<Product> listOfproducts) {
+            delegate.processFinish(listOfproducts);
+        }
     }
 
 
---
+++ app/src/main/java/eu/swipefit/app/networking/Networking.java
@@ -0,0 +1,167 @@
+package eu.swipefit.app.networking;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import eu.swipefit.app.Product;
+
+/**
+ * Created by GeorgeGabriel on 12/02/2017.
+ */
+
+public class Networking {
+
+    private static final String TAG = "Networking";
+    private static final String LOG_TAG = Networking.class.getSimpleName();
+
+    public static List<Product> loadProducts(Context context, String json){
+        try{
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            JSONArray array = new JSONArray(json);
+            List<Product> productList = new ArrayList<>();
+            for(int i=0;i<array.length();i++){
+                Product product = gson.fromJson(array.getString(i), Product.class);
+                productList.add(product);
+            }
+            return productList;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /*private static String loadJSONFromAsset(Context context, String jsonFileName) {
+        String json = null;
+        InputStream is=null;
+        try {
+            AssetManager manager = context.getAssets();
+            Log.d(TAG,"path "+jsonFileName);
+            is = manager.open(jsonFileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }*/
+
+    /**
+     * Query the USGS dataset and return a list of {@link Product} objects.
+     */
+    public static List<Product> fetchEarthquakeData(Context context, String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
+        List<Product> products = loadProducts(context, jsonResponse);
+
+        // Return the list of {@link Earthquake}s
+        return products;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+}
