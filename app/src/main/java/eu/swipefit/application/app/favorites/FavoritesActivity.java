565ed78732eb6fe49b109373c56d5174752f238b
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-23 13:43:56 +0300

    resolved empty activity if favorite product list is empty - added new layout to be displayed if there is this case

--- app/src/main/java/eu/swipefit/application/app/favorites/FavoritesActivity.java
+++ app/src/main/java/eu/swipefit/application/app/favorites/FavoritesActivity.java
@@ -9,18 +9,54 @@ import android.support.annotation.Nullable;
 import android.widget.ListView;
 
 import eu.swipefit.app.R;
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
 
 /** ADD COMMENTS */
-public class FavoritesActivity extends Activity {
+public class FavoritesActivity extends Activity implements SwipeBackActivityBase{
+
+    private SwipeBackActivityHelper swipeBackActivityHelper = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.favorites_list_activity);
+        // instantiate swipeBackActivityHelper right after the creation of the activity
+        swipeBackActivityHelper = new SwipeBackActivityHelper(this);
+        if(FavoritesContainer.getProducts().size() == 0 || FavoritesContainer.getProducts() == null) {
+            setContentView(R.layout.no_favorite_items);
+            // enable the swipeBackActivityHelper after the content view has been set
+            swipeBackActivityHelper.onActivityCreate();
+        }
+        else {
+            setContentView(R.layout.favorites_list_activity);
+            // enable the swipeBackActivityHelper after the content view has been set
+            swipeBackActivityHelper.onActivityCreate();
+            ProductAdapter productAdapter = new ProductAdapter(getApplicationContext(),0,FavoritesContainer.getProducts());
+            ListView listView = findViewById(R.id.listView);
 
+            listView.setAdapter(productAdapter);
+        }
+    }
 
-        ProductAdapter productAdapter = new ProductAdapter(getApplicationContext(),0,FavoritesContainer.getProducts());
-        ListView listView = findViewById(R.id.listView);
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        swipeBackActivityHelper.onPostCreate();
+    }
 
-        listView.setAdapter(productAdapter);
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return swipeBackActivityHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+
     }
 }
