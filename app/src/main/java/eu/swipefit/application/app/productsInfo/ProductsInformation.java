56b0e3c0fce9c9a3a42bcb3c345ef0ce2adde3bc
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-26 04:09:46 +0300

    added a Map that stores the id of a product and its score (like || dislike)

--- app/src/main/java/eu/swipefit/application/app/productsInfo/ProductsInformation.java
+++ app/src/main/java/eu/swipefit/application/app/productsInfo/ProductsInformation.java
@@ -3,13 +3,19 @@ package eu.swipefit.application.app.productsInfo;
  * FILE DESCRIPTION
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 /** ADD COMMENTS */
 public class ProductsInformation {
 
     // variable that stores the sate of the products (likes and dislikes)
     private static double[] productInformation = null;
+    private static HashMap<String,String> cardsIdInformation = new HashMap<>();
+
+    // this variable should be reinitialized after POST method
+    private static ArrayList<String> cardsIds = new ArrayList<>();
 
 
     public static void initiateInfoArray(int size) {
@@ -24,11 +30,17 @@ public class ProductsInformation {
     // if the user likes the item then we set metadata to 1
     public static void like(int index) {
         productInformation[index] = 1;
+        cardsIdInformation.put(cardsIds.get(index),"1");
     }
 
     // if the user doesn't like the item then we set metadata to 0
     public static void dislike(int index) {
         productInformation[index] = 0;
+        cardsIdInformation.put(cardsIds.get(index),"0");
+    }
+
+    public static void setCardId(String id) {
+        cardsIds.add(id);
     }
 
 
