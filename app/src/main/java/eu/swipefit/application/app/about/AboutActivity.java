dbcfa77524e00cbe2b5f3fd30f9939fe04215062
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-07-31 22:59:57 +0300

    added a new layout for about_activity and added a few more goodies to it; also added a few drawables

--- app/src/main/java/eu/swipefit/application/app/about/AboutActivity.java
+++ app/src/main/java/eu/swipefit/application/app/about/AboutActivity.java
@@ -4,41 +4,111 @@ package eu.swipefit.application.app.about;
  */
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 
 import eu.swipefit.app.R;
 import eu.swipefit.application.networking.PushData;
+import io.saeid.fabloading.LoadingView;
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 /** ADD COMMENTS */
-public class AboutActivity extends Activity{
+public class AboutActivity extends Activity implements SwipeBackActivityBase{
+
+    private SwipeBackActivityHelper swipeBackActivityHelper = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_app_activity);
-        /*KonfettiView view = (KonfettiView) findViewById(R.id.viewKonfetti);
+
+        /**
+         * Instantiate swipeBackActivityHelper right after the creation of the activity
+         * */
+
+        swipeBackActivityHelper = new SwipeBackActivityHelper(this);
+
+        /**
+         * Enable the swipeBackActivityHelper after the content view has been set
+         * */
+
+        swipeBackActivityHelper.onActivityCreate();
+
+        KonfettiView konfettiView00 = findViewById(R.id.viewKonfetti_00);
+        KonfettiView konfettiView01 = findViewById(R.id.viewKonfetti_01);
+
         Size size = new Size(12,3);
-        view.build()
+
+
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                LoadingView mLoadingView = findViewById(R.id.loading_view);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.shorts,LoadingView.FROM_TOP);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.sneakers,LoadingView.FROM_BOTTOM);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.sneakers,LoadingView.FROM_RIGHT);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.tie,LoadingView.FROM_LEFT);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.top_hat,LoadingView.FROM_RIGHT);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.trousers,LoadingView.FROM_TOP);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.tshirt,LoadingView.FROM_BOTTOM);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.vest,LoadingView.FROM_LEFT);
+                mLoadingView.addAnimation(R.color.colorBackground,R.drawable.hoodie,LoadingView.FROM_BOTTOM);
+                mLoadingView.startAnimation();
+            }
+        });
+
+        konfettiView00.build()
                 .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
                 .setDirection(0.0, 359.0)
                 .setSpeed(1f, 5f)
                 .setFadeOutEnabled(true)
-                .setTimeToLive(2000L)
+                .setTimeToLive(5000L)
                 .addShapes(Shape.RECT, Shape.CIRCLE)
-                .addSizes(size)
-                .setPosition(-50f, view.getWidth() + 50f, -50f, -50f)
-                .stream(300, 5000L);*/
+                .addSizes(new Size(12, 5f))
+                .setPosition(0, konfettiView00.getWidth() +1000f, 50f, 50f)
+                .stream(900, 5000L);
+
+        konfettiView01.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(5000L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(new Size(12, 5f))
+                .setPosition(0, konfettiView01.getWidth() +1000f, 600f, 10f)
+                .stream(900, 5000L);
     }
 
-    public void onButtonClick(View view) {
-        Button button = findViewById(R.id.POST_BUTTON);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PushData.sendUserBehaviourToServer();
-            }
-        });
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        swipeBackActivityHelper.onPostCreate();
     }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return swipeBackActivityHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+
+    }
+
 }
