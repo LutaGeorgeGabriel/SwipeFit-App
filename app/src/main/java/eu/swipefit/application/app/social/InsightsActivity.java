ddd272737873c694856908672e3d07d41de453eb
Luta George Gabriel <luta.george.gabriel@gmail.com>
2017-08-15 21:37:54 +0300

    refactoring - renamed activity

--- app/src/main/java/eu/swipefit/application/app/social/InsightsActivity.java
+++ app/src/main/java/eu/swipefit/application/app/social/InsightsActivity.java
@@ -6,24 +6,29 @@ package eu.swipefit.application.app.social;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import eu.swipefit.app.R;
+import eu.swipefit.application.app.sharedPreferences.SharedPreferencesCounter;
 import me.imid.swipebacklayout.lib.SwipeBackLayout;
 import me.imid.swipebacklayout.lib.Utils;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
 
 /** ADD COMMENTS */
-public class SocialActivity extends Activity implements SwipeBackActivityBase{
+public class InsightsActivity extends Activity implements SwipeBackActivityBase{
 
     private SwipeBackActivityHelper swipeBackActivityHelper;
+    private TextView sumOfLikes;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.social_activity);
+        setContentView(R.layout.insights_activity);
         swipeBackActivityHelper = new SwipeBackActivityHelper(this);
         swipeBackActivityHelper.onActivityCreate();
+        sumOfLikes = findViewById(R.id.sumOfLikes);
+        sumOfLikes.setText(String.valueOf(SharedPreferencesCounter.getDefaults("counter", getApplicationContext())));
     }
 
 
